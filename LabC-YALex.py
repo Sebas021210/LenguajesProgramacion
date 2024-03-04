7529e63868101a0080c454c7b376e34bb14e95a3
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-03-04 09:58:06 -0600

    Leer YALex

---
+++ LabC-YALex.py
@@ -0,0 +1,263 @@
+import graphviz
+import re
+
+def convert_optional(regex):
+    return regex.replace('?', '|E')
+
+def expandir_rango(rango):
+    inicio, fin = rango
+    return '|'.join([f'({chr(i)})' for i in range(ord(inicio), ord(fin) + 1)])
+
+def expandir_extensiones(expresion):
+    patron_extension = re.compile(r'\[([^\]]+)\]')
+    coincidencias = patron_extension.findall(expresion)
+
+    for coincidencia in coincidencias:
+        rangos = coincidencia.split(' ')
+        expresion_rangos = []
+        for rango in rangos:
+            if '-' in rango:
+                inicio, fin = rango.split('-')
+                if len(inicio) == 1 and len(fin) == 1:
+                    expresion_rangos.append(expandir_rango((inicio, fin)))
+                elif len(inicio) == 3 and inicio[0] == "'" and inicio[2] == "'" and len(fin) == 3 and fin[0] == "'" and fin[2] == "'":
+                    expresion_rangos.append(expandir_rango((inicio[1], fin[1])))
+            else:
+                expresion_rangos.append(rango)
+        
+        expresion = expresion.replace(f'[{coincidencia}]', f'({"|".join(expresion_rangos)})')
+    
+    return expresion
+
+def convertir_expresion(expresion):
+    lista = list(expresion)
+    alfabeto = []
+    operandos = ['+','.','*','|','(',')','[',']','{','}','?']
+
+    for i in lista:
+        if i not in operandos:
+            if i not in alfabeto:
+                alfabeto.append(i)
+
+    alfabeto.append('')
+    
+    for i in range(len(lista)):
+        if i > 0:
+            before = lista[i - 1]
+            if lista[i] == '+':
+                if before not in ')]}':
+                    lista[i - 1] = lista[i - 1] + lista[i - 1] + '*'
+                else:
+                    almacen = []
+                    aperturas = 0
+                    for j in range(i - 1, -1, -1):
+                        if lista[j] in ')]}':
+                            aperturas += 1
+                            almacen.append(lista[j])
+                        elif lista[j] in '([{':
+                            aperturas -= 1
+                            almacen.append(lista[j])
+                        else:
+                            almacen.append(lista[j])
+                        if aperturas == 0:
+                            break
+                    almacen.reverse()
+                    lista[i] = ''.join(almacen) + '*'
+    while '+' in lista:
+        lista.remove('+')
+    return ''.join(lista), alfabeto
+
+def concatenacion(exp: str) -> str:
+    nueva_exp = ''
+    for i in range(len(exp) - 1):
+        if exp[i] not in ['(', '|', '.'] and exp[i + 1] not in [')', '|', '*', '.']:
+            nueva_exp += exp[i] + '.'
+        else:
+            nueva_exp += exp[i] 
+
+    nueva_exp += exp[-1] 
+
+    return nueva_exp
+
+def infix_postfix(infix):
+    caracteres_especiales = {'*': 60, '.': 40, '|': 20}
+    exp_postfix, stack = "", ""  
+
+    for c in infix:        
+        if c == '(':
+            stack = stack + c 
+        elif c == ')':
+            while stack[-1] != '(':  
+                exp_postfix = exp_postfix + stack[-1]  
+                stack = stack[:-1]  
+            stack = stack[:-1]  
+        elif c in caracteres_especiales:
+            while stack and caracteres_especiales.get(c, 0) <= caracteres_especiales.get(stack[-1], 0):
+                exp_postfix, stack = exp_postfix + stack[-1], stack[:-1]
+            stack = stack + c
+        else:
+            exp_postfix = exp_postfix + c
+
+    while stack:
+        exp_postfix, stack = exp_postfix + stack[-1], stack[:-1]
+
+    return exp_postfix
+
+class Estado:
+    def __init__(self):
+        self.label = None
+        self.transicion1 = None
+        self.transicion2 = None
+        self.etiqueta = None
+        self.anulable = None
+        self.primera_pos = None
+        self.ultima_pos = None
+        self.siguiente_pos = set()
+
+nodo_etiqueta = 1
+nodos = {}
+
+def aumentar_expresion(expresion):
+    return f"{expresion}#."
+
+def construir_AS(exp_aumentada):
+    global nodo_etiqueta 
+    global nodos
+    stack = []
+    i = 0
+    while i < len(exp_aumentada):
+        char = exp_aumentada[i]
+        if char == 'E':
+            nodo = Estado()
+            nodo.label = char
+            nodo.anulable = True
+            nodo.primera_pos = set()
+            nodo.ultima_pos = set()
+            stack.append(nodo)
+        elif char.isalpha() or char == '#' or char.isalnum() and char != 'E':
+            nodo = Estado()
+            nodo.label = char
+            nodo.etiqueta = nodo_etiqueta  
+            nodo.anulable = False
+            nodo.primera_pos = {nodo_etiqueta}
+            nodo.ultima_pos = {nodo_etiqueta}
+            nodos[nodo_etiqueta] = nodo
+            nodo_etiqueta += 1 
+            stack.append(nodo)
+        elif char in "|.*":
+            nodo = Estado()
+            nodo.label = char
+            if char == '*':
+                c1 = stack.pop()
+                nodo.transicion1 = c1
+                nodo.anulable = True 
+                nodo.primera_pos = c1.primera_pos
+                nodo.ultima_pos = c1.ultima_pos
+                for pos in c1.ultima_pos:
+                    nodos[pos].siguiente_pos.update(c1.primera_pos)
+            else:
+                c2 = stack.pop()
+                c1 = stack.pop()
+                if char == '|':
+                    nodo.anulable = c1.anulable or c2.anulable
+                    nodo.primera_pos = c1.primera_pos.union(c2.primera_pos)
+                    nodo.ultima_pos = c1.ultima_pos.union(c2.ultima_pos)
+                else: 
+                    nodo.anulable = c1.anulable and c2.anulable 
+                    if c1.anulable:
+                        nodo.primera_pos = c1.primera_pos.union(c2.primera_pos)
+                    else:
+                        nodo.primera_pos = c1.primera_pos
+                    if c2.anulable:
+                        nodo.ultima_pos = c1.ultima_pos.union(c2.ultima_pos)
+                    else:
+                        nodo.ultima_pos = c2.ultima_pos
+                    for pos in c1.ultima_pos:
+                        nodos[pos].siguiente_pos.update(c2.primera_pos)
+                nodo.transicion2 = c2
+                nodo.transicion1 = c1
+            stack.append(nodo)
+        i += 1
+
+    return stack.pop()
+
+def construir_transiciones(arbol, expresion):
+    transiciones = set(expresion) - set(['|', '*', '(', ')', '#', 'ε'])
+    estados = {frozenset(arbol.primera_pos): 's0'}
+    transiciones_estados = {'s0': {}}
+    pendientes = [arbol.primera_pos]
+    estado_aceptacion = max(nodos.keys())
+    while pendientes:
+        actual = pendientes.pop()
+        estado_actual = estados[frozenset(actual)]
+        for transicion in transiciones:
+            nuevo = set()
+            for pos in actual:
+                nodo = nodos[pos]
+                if nodo.label == transicion:
+                    nuevo.update(nodo.siguiente_pos)
+            if nuevo:
+                estado_nuevo = estados.get(frozenset(nuevo))
+                if estado_nuevo is None:
+                    estado_nuevo = f's{len(estados)}'
+                    estados[frozenset(nuevo)] = estado_nuevo
+                    pendientes.append(nuevo)
+                if estado_actual not in transiciones_estados:
+                    transiciones_estados[estado_actual] = {}
+                transiciones_estados[estado_actual][transicion] = estado_nuevo
+    return estados, transiciones_estados, estado_aceptacion
+
+def graficar_afd_directo(estados, transiciones):
+    dot = graphviz.Digraph(format='png')
+
+    estado_aceptacion = max(nodos.keys()) 
+    estados_invertidos = {v: k for k, v in estados.items()}
+    for estado, transiciones_estado in transiciones.items():
+        for transicion, estado_destino in transiciones_estado.items():
+            dot.edge(estado, estado_destino, label=transicion)
+        if estado_aceptacion in estados_invertidos[estado]:
+            dot.node(estado, shape='doublecircle')
+
+    dot.render('afd_graph_directo', view=True)
+
+def leer_yalex(nombre_archivo):
+    with open(nombre_archivo, 'r') as archivo:
+        contenido = archivo.read()
+    return contenido
+
+def AFD_yalex(yalex_contenido):
+    expresiones = re.findall(r'let\s+(\w+)\s*=\s*(.+)', yalex_contenido)
+    print("Expresiones: ")
+    for i in range(len(expresiones)):
+        print(expresiones[i])
+
+    expresiones_dict = dict(expresiones)
+
+    def reemplazar_referencias(exp):
+        for nombre, valor in expresiones_dict.items():
+            exp = exp.replace(nombre, f'({valor})')
+        return exp
+
+    expresion_completa = '|'.join([f'({reemplazar_referencias(exp)})' for _, exp in expresiones])
+    print("\nExpresion completa: ")
+    print(expresion_completa)
+
+    infix = convert_optional(expresion_completa)
+    infix = expandir_extensiones(infix)
+    infix, alfabeto = convertir_expresion(infix)
+    exp_explicita = concatenacion(infix)
+    postfix = infix_postfix(exp_explicita)
+
+    exp_aumentada = aumentar_expresion(postfix)
+    print("\nExpresion aumentada: ")
+    print(exp_aumentada)
+
+    arbol_sintactico = construir_AS(exp_aumentada)
+    estados, transiciones, estado_aceptacion = construir_transiciones(arbol_sintactico, exp_aumentada)
+
+    return estados, transiciones, estado_aceptacion
+
+nombre_archivo = './YALex/slr-0.yal'
+yalex = leer_yalex(nombre_archivo)
+estados, transiciones, estado_aceptacion = AFD_yalex(yalex)
+graficar_afd_directo(estados, transiciones)
